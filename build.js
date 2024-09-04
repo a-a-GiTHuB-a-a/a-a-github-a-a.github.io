8172903d6534ea7a64ef60dbe64c56210e5755f2
a-a-GiTHuB-a-a <64599476+a-a-GiTHuB-a-a@users.noreply.github.com>
2024-09-04 13:49:20 -0400

    Add a build script for convenience.

---
+++ build.js
@@ -0,0 +1,75 @@
+const fs = require("node:fs");
+const path = require("node:path");
+const ejs = require("ejs");
+
+if (process.argv.length != 4) {
+	console.error("Fatal Error: must have 4 args");
+	process.exit(1);
+}
+
+const source_dir = process.argv[2];
+const build_dir = process.argv[3];
+
+if (path.relative(__dirname, build_dir).includes("..")) {
+	console.error("Fatal error: Attempted to modify files outside of this program's scope.");
+	process.exit(1);
+}
+
+function process_dir(dir_path) {
+	const source_path = path.join(source_dir, dir_path);
+	const build_path = path.join(build_dir, dir_path);
+
+	const to_read = fs.opendirSync(source_path);
+	fs.mkdirSync(build_path);
+
+	while (true) {
+		const item = to_read.readSync();
+
+		if (item == null) {
+			break;
+		} else if (item.isDirectory()) {
+			process_dir(path.join(dir_path, item.name));
+		} else {
+			process_file(path.join(dir_path, item.name));
+		}
+	}
+	
+	to_read.closeSync();
+}
+
+function process_file(file_path) {
+	const source_path = path.join(source_dir, file_path);
+	let build_path = path.join(build_dir, file_path);
+
+	const file = fs.readFileSync(source_path, {encoding: "utf8"});
+	let new_content;
+
+	switch (path.extname(source_path)) {
+		case ".ejs": {
+			new_content = ejs.render(template, {
+				"body": file
+			}, {
+				filename: file_path,
+				rmWhitespace: true,
+			});
+			let path_obj = path.parse(build_path);
+			path_obj.ext = ".html";
+			path_obj.base = path_obj.base.replace(/\.\w+/, ".html");
+			build_path = path.format(path_obj);
+			break;
+		}
+		default: {
+			new_content = file;
+			break;
+		}
+	}
+
+	fs.writeFileSync(build_path, new_content);
+}
+
+const template = fs.readFileSync("./template.ejs", {encoding: "utf8"});
+fs.rmSync(build_dir, {
+	recursive: true,
+	force: true,
+});
+process_dir(".");
