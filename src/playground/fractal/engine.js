8696d6e3c72a958889f4a29aa0ac35e2c47e8900
a-a-GiTHuB-a-a <64599476+a-a-GiTHuB-a-a@users.noreply.github.com>
2024-09-07 11:48:01 -0400

    added thing to build, and made a script playground

---
+++ src/playground/fractal/engine.js
@@ -0,0 +1,117 @@
+class Fractal {
+	scale;
+	depth;
+	rotation;
+	commands;
+
+	Fractal(scale, depth, rotation, commands) {
+		this.scale = scale;
+		this.depth = depth;
+		this.rotation = rotation;
+		this.commands = commands;
+	}
+}
+
+function t(strings, ...values) {
+	s = strings[0];
+	let i = 0;
+	while (true) {
+		try {
+			s += values[i];
+			s += strings[i+1];
+			i++;
+		} catch {
+			break;
+		}
+	}
+}
+
+function re(flags) {
+	return (...args) => new RegExp(t(...args), flags);
+}
+
+let current_fractal = new Fractal(100, 5);
+
+const num_re = /\d+(\.\d+)?/;
+const line_sep_re = /\s+;\s+/m;
+const var_re = /(?<varname>[a-z]+)\s+=\s+(?<value>[a-z]+)/m;
+const cmd_re = re("m")`(?<cmdname>[a-z]+)\s+(?<value>${num_re})`;
+
+$("#newfrac").on("submit", async function(e) {
+	e.preventDefault();
+	current_fractal = Compile(await $("#fracfile")[0].files[0].text());
+});
+
+async function Compile(contents) {
+	contents = contents.trim();
+	let lines = contents.split(line_sep_re);
+	let frac = new Fractal();
+	let initial_scale = 100;
+	let depth = 5;
+	let cmds = [];
+	for (let line in lines) {
+		let assign = var_re.exec(line);
+		if (assign !== null) {
+			switch (assign.groups.varname) {
+				case "initialscale":
+				case "initial_scale":
+				case "initial-scale": {
+					initial_scale = +assign.groups.value;
+					break;
+				}
+				case "depth": {
+					depth = +assign.groups.value;
+					break;
+				}
+				case "rotation": {
+					rotation = +assign.groups.value;
+					break;
+				}
+			}
+			continue;
+		}
+		let cmd = cmd_re.exec(line);
+		if (cmd !== null) {
+			cmds.push({
+				name: cmd.groups.cmdname.toLowerCase(),
+				value: cmd.groups.value
+			});
+		}
+	}
+	frac.scale = initial_scale;
+	frac.depth = depth;
+	frac.rotation = rotation;
+	frac.commands = cmds;
+	return frac;
+}
+
+function Draw(position, fractal) {
+	let scale = fractal.scale;
+	let rotation = fractal.rotation;
+	let depth = fractal.depth;
+	if (depth == 0) {
+		return Path.Line({
+			from: position,
+			to: [
+				position[0] + scale * Math.cos(rotation),
+				position[1] + scale * Math.sin(rotation)
+			]
+		});
+	} else {
+		depth--;
+		let p = new Path().add(position);
+		for (let command of fractal.commands) {
+			switch (command.name) {
+				case "rotate": {
+					rotation += command.value;
+					break;
+				}
+				case "line": {
+					scale *= command.value;
+					p.addSegments(Draw(p, new Fractal(scale, depth, rotation, commands)).segments);
+					break;
+				}
+			}
+		}
+	}
+}
