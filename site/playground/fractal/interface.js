28e1c6b86541efd1f1a6433d02257329f9e8434a
a-a-GiTHuB-a-a <64599476+a-a-GiTHuB-a-a@users.noreply.github.com>
2024-09-13 12:40:34 -0400

    remove site dir from my local build

--- site/playground/fractal/interface.js
+++
@@ -1,8 +0,0 @@
-$("#newfrac").on("submit", function(e) {
-	e.preventDefault();
-	e.stopPropagation();
-	const file = $("#fracfile")[0].files[0];
-	file.text().then((data) => {
-		current_fractal = Compile(data);
-	});
-});
