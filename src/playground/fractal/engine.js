3681180a350239939188030cd8e058ffead3f5e3
a-a-GiTHuB-a-a <64599476+a-a-GiTHuB-a-a@users.noreply.github.com>
2024-09-17 12:48:26 -0400

    double log

--- src/playground/fractal/engine.js
+++ src/playground/fractal/engine.js
@@ -118,7 +118,9 @@ function Draw(fractal, config) {
 				}
 				case "line": {
 					fractal.scale *= command.value;
+					console.log(fractal);
 					const subsegs = Draw(structuredClone(fractal), config).segments;
+					console.log(fractal);
 					p.addSegments(subsegs.slice(1));
 					fractal.position = subsegs[subsegs.length-1].point;
 				}
