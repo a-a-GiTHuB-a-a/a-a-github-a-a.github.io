6455fcd2d6aaadfccc14f53224950500bb28241c
a-a-GiTHuB-a-a <64599476+a-a-GiTHuB-a-a@users.noreply.github.com>
2024-09-17 13:08:16 -0400

    Quick favicon detour

---
+++ src/logo.js
@@ -0,0 +1,16 @@
+$(function() {
+	const date = new Date();
+	const month = date.getMonth();
+	const day = date.getDate();
+	let icon;
+
+	if (month === 12 && (24 <= day) && (26 >= day)) {
+		icon = "BOXEN Logo Christmas.png";
+	} else if (month === 11) {
+		icon = "BOXEN Logo Thanksgiving.png";
+	} else {
+		icon = "BOXEN Logo.png";
+	}
+
+	$("#favicon").attr("href", icon);
+});
