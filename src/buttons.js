8fdecf5b55f56680e4634f17d777af072a554857
a-a-GiTHuB-a-a <64599476+a-a-GiTHuB-a-a@users.noreply.github.com>
2024-09-23 17:00:46 -0400

    glint? maybe?

--- src/buttons.js
+++ src/buttons.js
@@ -1,11 +1,16 @@
 $(function() {
 	const buttons = $("button,input[type='button'],input[type='submit']");
+	buttons.attr("glintPosition", "0%");
 	function animate() {
 		buttons.animate({
 			glintPosition: "100%",
 		}, {
 			step(_, tween) {
-				console.log(tween);
+				$(this).css("background-image", `linear-gradient(${this.css("background-color")}, #9f9f9f ${_}%, ${this.css("background-color")})`)
+			},
+			finally() {
+				$(this).style("glintPosition", "0%");
+				animate();
 			}
 		});
 	}
