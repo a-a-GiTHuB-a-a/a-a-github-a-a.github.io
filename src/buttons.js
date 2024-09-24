2e6f5de2225e8d728b0f50945c0c7ee278d29ecf
a-a-GiTHuB-a-a <64599476+a-a-GiTHuB-a-a@users.noreply.github.com>
2024-09-24 11:07:34 -0400

    even more optimization

--- src/buttons.js
+++ src/buttons.js
@@ -6,8 +6,12 @@ $(function() {
 			step(val, _) {
 				const color = elem.css("background-color");
 				if (val < 0) {
-					elem.css("background-image", `linear-gradient(90deg, ${color} ${val-10}%, #9f9f9f ${val}%, ${color} ${val+10}%, ${color})`);
+					elem.css("background-image", `linear-gradient(90deg, #9f9f9f ${val}%, ${color} ${val+10}%, ${color})`);
 				} else if (val > 100) {
+					elem.css("background-image", `linear-gradient(90deg, ${color}, ${color} ${val-10}%, #9f9f9f ${val}%)`);
+				} else if (val < 10) {
+					elem.css("background-image", `linear-gradient(90deg, ${color} ${val-10}%, #9f9f9f ${val}%, ${color} ${val+10}%, ${color})`);
+				} else if (val > 90) {
 					elem.css("background-image", `linear-gradient(90deg, ${color}, ${color} ${val-10}%, #9f9f9f ${val}%, ${color} ${val+10}%)`);
 				} else {
 					elem.css("background-image", `linear-gradient(90deg, ${color}, ${color} ${val-10}%, #9f9f9f ${val}%, ${color} ${val+10}%, ${color})`);
