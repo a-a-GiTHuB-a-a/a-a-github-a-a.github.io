76d8f26e5ae9ebdd87434064aaedf1cd24a1004c
a-a-GiTHuB-a-a <64599476+a-a-GiTHuB-a-a@users.noreply.github.com>
2024-09-23 21:41:21 -0400

    forgot to wrap

--- src/buttons.js
+++ src/buttons.js
@@ -6,7 +6,7 @@ $(function() {
 			glintPosition: "100%",
 		}, {
 			step(_, tween) {
-				$(this).css("background-image", `linear-gradient(${this.css("background-color")}, #9f9f9f ${_}%, ${this.css("background-color")})`)
+				$(this).css("background-image", `linear-gradient(${$(this).css("background-color")}, #9f9f9f ${_}%, ${this.css("background-color")})`)
 			},
 			finally() {
 				$(this).style("glintPosition", "0%");
