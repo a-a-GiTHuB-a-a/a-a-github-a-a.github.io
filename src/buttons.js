58d3fae2b01a3ac5a8eca998f91aeff4934f90f7
a-a-GiTHuB-a-a <64599476+a-a-GiTHuB-a-a@users.noreply.github.com>
2024-09-24 10:55:52 -0400

    90deg

--- src/buttons.js
+++ src/buttons.js
@@ -4,7 +4,7 @@ $(function() {
 	function animate(elem) {
 		$({glintPosition: "0%"}).animate({glintPosition: "100%"}, {
 			step(val, _) {
-				elem.css("background-image", `linear-gradient(${elem.css("background-color")}, #9f9f9f ${val}%, ${elem.css("background-color")}`)
+				elem.css("background-image", `linear-gradient(90deg, ${elem.css("background-color")}, #9f9f9f ${val}%, ${elem.css("background-color")}`)
 			},
 			finally() {
 				animate(elem);
