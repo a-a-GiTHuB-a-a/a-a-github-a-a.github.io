283b966f763928b70a208b684d194f845513fe92
a-a-GiTHuB-a-a <64599476+a-a-GiTHuB-a-a@users.noreply.github.com>
2024-09-24 11:16:51 -0400

    generalize width

--- src/buttons.js
+++ src/buttons.js
@@ -1,17 +1,17 @@
 $(function() {
 	const buttons = $("button,input[type='button'],input[type='submit']");
 	function animate(elem) {
-		$({glintPosition: "-10"}).animate({glintPosition: "110"}, {
-			duration: 1000,
+		$({glintPosition: -animate.width}).animate({glintPosition: 100 + animate.width}, {
+			duration: 2000,
 			step(val, _) {
 				const color = elem.css("background-color");
 				if (val < 0) {
 					elem.css("background-image", `linear-gradient(90deg, #9f9f9f ${val}%, ${color} ${val+10}%, ${color})`);
 				} else if (val > 100) {
 					elem.css("background-image", `linear-gradient(90deg, ${color}, ${color} ${val-10}%, #9f9f9f ${val}%)`);
-				} else if (val < 10) {
+				} else if (val < animate.width) {
 					elem.css("background-image", `linear-gradient(90deg, ${color} ${val-10}%, #9f9f9f ${val}%, ${color} ${val+10}%, ${color})`);
-				} else if (val > 90) {
+				} else if (val > (100 - animate.width)) {
 					elem.css("background-image", `linear-gradient(90deg, ${color}, ${color} ${val-10}%, #9f9f9f ${val}%, ${color} ${val+10}%)`);
 				} else {
 					elem.css("background-image", `linear-gradient(90deg, ${color}, ${color} ${val-10}%, #9f9f9f ${val}%, ${color} ${val+10}%, ${color})`);
@@ -19,7 +19,8 @@ $(function() {
 			}
 		});
 	}
+	animate.width = 20;
 	buttons.each(function() {
-		setInterval(animate, 1500, $(this));
+		setInterval(animate, 2500, $(this));
 	});
 });
