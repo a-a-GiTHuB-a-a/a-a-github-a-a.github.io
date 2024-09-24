e63f7a36ceade8dbd9745a19b4d73c54e94bf22a
a-a-GiTHuB-a-a <64599476+a-a-GiTHuB-a-a@users.noreply.github.com>
2024-09-23 21:53:12 -0400

    take arg

--- src/buttons.js
+++ src/buttons.js
@@ -1,18 +1,18 @@
 $(function() {
 	const buttons = $("button,input[type='button'],input[type='submit']");
 	buttons.attr("glintPosition", "0%");
-	function animate() {
-		buttons.animate({
+	function animate(elem) {
+		elem.animate({
 			glintPosition: "100%",
 		}, {
-			step(_, tween) {
-				$(this).css("background-image", `linear-gradient(${$(this).css("background-color")}, #9f9f9f ${_}%, ${$(this).css("background-color")})`)
+			step(val, _) {
+				$(this).css("background-image", `linear-gradient(${$(this).css("background-color")}, #9f9f9f ${val}%, ${$(this).css("background-color")})`)
 			},
 			finally() {
 				$(this).style("glintPosition", "0%");
-				animate();
+				animate($(this));
 			}
 		});
 	}
-	animate();
+	animate(buttons);
 });
