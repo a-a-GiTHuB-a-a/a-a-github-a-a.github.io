3dbacf057a0ca1842b9228c9d84f82e299e6ff1f
a-a-GiTHuB-a-a <64599476+a-a-GiTHuB-a-a@users.noreply.github.com>
2024-10-07 14:49:06 -0400

    change template because surely that'll help

--- build.js
+++ build.js
@@ -15,7 +15,7 @@ if (path.relative(__dirname, build_dir).includes("..")) {
 	process.exit(1);
 }
 
-async function process_dir(dir_path) {
+async function process_dir(dir_path, opts) {
 	console.log(`Checking directory ${dir_path}`);
 	const source_path = path.join(source_dir, dir_path);
 	const build_path = path.join(build_dir, dir_path);
@@ -25,16 +25,16 @@ async function process_dir(dir_path) {
 
 	for await (let item of to_read) {
 		if (item.isDirectory()) {
-			await process_dir(path.join(dir_path, item.name));
+			await process_dir(path.join(dir_path, item.name), opts);
 		} else {
-			await process_file(path.join(dir_path, item.name));
+			await process_file(path.join(dir_path, item.name), opts);
 		}
 	}
 	
 	await to_read.close();
 }
 
-async function process_file(file_path) {
+async function process_file(file_path, opts) {
 	console.log(`Building file ${file_path}`);
 	const source_path = path.join(source_dir, file_path);
 	let build_path = path.join(build_dir, file_path);
@@ -44,7 +44,7 @@ async function process_file(file_path) {
 	switch (path.extname(source_path)) {
 		case ".ejs": {
 			const metadata = JSON.parse(await fsPromises.readFile(source_path.replace(".ejs", ".json"), {encoding: "utf8"}));
-			const new_content = ejs.render(template, {
+			const new_content = ejs.render(opts.ejs_template, {
 				...metadata,
 				"body": file
 			}, {
@@ -67,4 +67,4 @@ async function process_file(file_path) {
 	}
 }
 
-fsPromises.readFile("./template.ejs", {encoding: "utf8"}).then(template => process_dir("."));
+fsPromises.readFile("./template.ejs", {encoding: "utf8"}).then(template => process_dir(".", {ejs_template: template}));
