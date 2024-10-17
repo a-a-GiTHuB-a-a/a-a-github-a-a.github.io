baf52ead59ba7e6ca75eab94346068153206cb38
a-a-GiTHuB-a-a <64599476+a-a-GiTHuB-a-a@users.noreply.github.com>
2024-10-17 11:05:10 -0400

    reformatting

--- src/schemacell/docs/auto.js
+++ src/schemacell/docs/auto.js
@@ -1,21 +1,23 @@
-const toc_0 = $("#toc").append("<ol></ol>");
-let toc_1;
-let toc_2;
-let toc_3;
-let toc_4;
-let toc_5;
-let toc_6;
-$("*").each(function() {
-	switch($(this).prop("tagName")) {
-		case "h1": {
-			toc_1 = $(`<li><a href = "#${$(this).attr("id")}">${$(this).html()}</a></li>`);
-			toc_0.append(toc_1);
-			break;
+$(function() {
+	const toc_0 = $("#toc").append("<ol></ol>");
+	let toc_1;
+	let toc_2;
+	let toc_3;
+	let toc_4;
+	let toc_5;
+	let toc_6;
+	$("*").each(function() {
+		switch($(this).prop("tagName")) {
+			case "h1": {
+				toc_1 = $(`<li><a href = "#${$(this).attr("id")}">${$(this).html()}</a></li>`);
+				toc_0.append(toc_1);
+				break;
+			}
+			case "h2": {
+				toc_2 = $(`<li><a href = "#${$(this).attr("id")}">${$(this).html()}</a></li>`);
+				toc_1.append(toc_2);
+				break;
+			}
 		}
-		case "h2": {
-			toc_2 = $(`<li><a href = "#${$(this).attr("id")}">${$(this).html()}</a></li>`);
-			toc_1.append(toc_2);
-			break;
-		}
-	}
+	});
 });
