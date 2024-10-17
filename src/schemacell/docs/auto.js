85a334710185417607f08a12283dfd0a807b32c6
a-a-GiTHuB-a-a <64599476+a-a-GiTHuB-a-a@users.noreply.github.com>
2024-10-17 11:13:22 -0400

    it's capital!?

--- src/schemacell/docs/auto.js
+++ src/schemacell/docs/auto.js
@@ -7,16 +7,35 @@ $(function() {
 	let toc_5;
 	let toc_6;
 	$("*").each(function() {
-		console.log(this.tagName);
 		switch(this.tagName) {
-			case "h1": {
-				toc_1 = $(`<li><a href = "#${$(this).attr("id")}">${$(this).html()}</a></li>`);
+			case "H1": {
+				toc_1 = $(`<li><a href = "#${this.id}">${$(this).html()}</a></li>`);
 				toc_0.append(toc_1);
 				break;
 			}
-			case "h2": {
-				toc_2 = $(`<li><a href = "#${$(this).attr("id")}">${$(this).html()}</a></li>`);
+			case "H2": {
+				toc_2 = $(`<li><a href = "#${this.id}">${$(this).html()}</a></li>`);
 				toc_1.append(toc_2);
+				break;
+			}
+			case "H3": {
+				toc_3 = $(`<li><a href = "#${this.id}">${$(this).html()}</a></li>`);
+				toc_2.append(toc_3);
+				break;
+			}
+			case "H4": {
+				toc_4 = $(`<li><a href = "#${this.id}">${$(this).html()}</a></li>`);
+				toc_3.append(toc_4);
+				break;
+			}
+			case "H5": {
+				toc_5 = $(`<li><a href = "#${this.id}">${$(this).html()}</a></li>`);
+				toc_4.append(toc_5);
+				break;
+			}
+			case "H6": {
+				toc_6 = $(`<li><a href = "#${this.id}">${$(this).html()}</a></li>`);
+				toc_5.append(toc_6);
 				break;
 			}
 		}
