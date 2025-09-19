1c9a196bb76d298032da37cec577a797e696df29
a-a-GiTHuB-a-a <64599476+a-a-GiTHuB-a-a@users.noreply.github.com>
2025-09-18 22:52:29 -0400

    instantiate tool

--- src/playground/fractal/engine.ts
+++ src/playground/fractal/engine.ts
@@ -4,6 +4,8 @@ import paper from "paper";
 import $ from "jquery";
 
 paper.setup($("canvas#content")[0] as HTMLCanvasElement);
+const tool = new paper.Tool();
+tool.activate();
 
 type StyleConfig = Partial<Omit<paper.Style, "view">>;
 
