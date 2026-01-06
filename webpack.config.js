a325a12986120b597bf76463a5c50a2dbd078c91
a-a-GiTHuB-a-a <64599476+a-a-GiTHuB-a-a@users.noreply.github.com>
2026-01-06 13:17:48 -0500

    no double extension

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const isProduction = process.env.NODE_ENV == "production";
 const config = {
     entry: {
         frac: {
-            import: path.resolve(__dirname, "src/playground/fractal/engine.ts"),
+            import: path.resolve(__dirname, "src/playground/fractal/engine"),
             filename: "./playground/fractal/engine.js",
         },
     },
