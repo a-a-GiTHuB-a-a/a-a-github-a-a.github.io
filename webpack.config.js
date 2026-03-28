7449ba387f6e28dd50dc3a19d265a90cc2cfddb8
a-a-GiTHuB-a-a <64599476+a-a-GiTHuB-a-a@users.noreply.github.com>
2026-03-28 17:04:07 -0400

    bruh im a dumb

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ const config = {
         ],
     },
     resolve: {
-        extensions: [".tsx", ".ts",/* ".jsx", ".js"*/, "..."],
+        extensions: [".tsx", ".ts", /* ".jsx", ".js",*/"..."],
     },
 };
 
