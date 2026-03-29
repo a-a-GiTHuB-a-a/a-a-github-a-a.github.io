fa43922dfbcf5db9ea0614039fc5f69d256a818b
a-a-GiTHuB-a-a <64599476+a-a-GiTHuB-a-a@users.noreply.github.com>
2026-03-29 13:05:25 -0400

    use value

--- src/playground/golf/clua/page_handler.ts
+++ src/playground/golf/clua/page_handler.ts
@@ -1,7 +1,7 @@
 import * as code_handler from "./code_handler";
 
 $("#clua").on("input change", function(e) {
-	$("#code-info").text(`${code_handler.count_chars(this.innerText)} characters, ${code_handler.count_bytes(this.innerText)} bytes`);
+	$("#code-info").text(`${code_handler.count_chars((this as HTMLTextAreaElement).value)} characters, ${code_handler.count_bytes((this as HTMLTextAreaElement).value)} bytes`);
 });
 $("#transpile").on("click", function(e:JQuery.ClickEvent) {
 	$("#lua").val(code_handler.decompress($("#clua").val().toString()));
