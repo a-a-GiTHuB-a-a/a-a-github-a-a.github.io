b343717e1f611e59f9e9a0842bc6328616803c36
a-a-GiTHuB-a-a <64599476+a-a-GiTHuB-a-a@users.noreply.github.com>
2026-03-29 13:13:12 -0400

    extract code-getting

--- src/playground/golf/clua/page_handler.ts
+++ src/playground/golf/clua/page_handler.ts
@@ -1,7 +1,8 @@
 import * as code_handler from "./code_handler";
 
 $("#clua").on("input change", function(e) {
-	$("#code-info").text(`${code_handler.count_chars((this as HTMLTextAreaElement).value)} characters, ${code_handler.count_bytes((this as HTMLTextAreaElement).value)} bytes`);
+	let code = (this as HTMLTextAreaElement).value;
+	$("#code-info").text(`${code_handler.count_chars(code)} characters, ${code_handler.count_bytes(code)} bytes`);
 });
 $("#transpile").on("click", function(e:JQuery.ClickEvent) {
 	$("#lua").val(code_handler.decompress($("#clua").val().toString()));
