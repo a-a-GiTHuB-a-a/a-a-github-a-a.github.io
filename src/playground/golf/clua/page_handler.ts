2a07c8a65555f8a4b0ae42a6f9c9821d46878cc3
a-a-GiTHuB-a-a <64599476+a-a-GiTHuB-a-a@users.noreply.github.com>
2026-03-29 13:55:53 -0400

    wait i wasn't paying attention what changed

--- src/playground/golf/clua/page_handler.ts
+++ src/playground/golf/clua/page_handler.ts
@@ -1,28 +1,30 @@
 import * as code_handler from "./code_handler";
 
-for (let char in code_handler.simple_substitutions) {
-	$("#chars").append($(`<button class = "symb">${char}</button>`));
-}
+$(function() { //does nothing. i just like having it all bundled up and cozy <3
+	for (let char in code_handler.simple_substitutions) {
+		$("#chars").append($(`<button class = "symb">${char}</button>`));
+	}
 
-$("#clua").on("input change", function(e) {
-	let code = (this as HTMLTextAreaElement).value;
-	$("#code-info").text(`${code_handler.count_chars(code)} characters, ${code_handler.count_bytes(code)} bytes`);
-});
-$("#transpile").on("click", function(e:JQuery.ClickEvent) {
-	$("#lua").val(code_handler.decompress($("#clua").val().toString()));
-});
-$(".symb").on("click", function(e) {
-	let clua = document.getElementById("clua") as HTMLTextAreaElement;
-	let char = this.innerText;
+	$("#clua").on("input change", function(e) {
+		let code = (this as HTMLTextAreaElement).value;
+		$("#code-info").text(`${code_handler.count_chars(code)} characters, ${code_handler.count_bytes(code)} bytes`);
+	});
+	$("#transpile").on("click", function(e:JQuery.ClickEvent) {
+		$("#lua").val(code_handler.decompress($("#clua").val().toString()));
+	});
+	$(".symb").on("click", function(e) {
+		let clua = document.getElementById("clua") as HTMLTextAreaElement;
+		let char = this.innerText;
 
-	let start = clua.selectionStart;
-	let end = clua.selectionEnd;
-	let text = clua.value;
+		let start = clua.selectionStart;
+		let end = clua.selectionEnd;
+		let text = clua.value;
 
-	clua.value = text.slice(0, start) + char + text.slice(end);
+		clua.value = text.slice(0, start) + char + text.slice(end);
 
-	// Move cursor after inserted text
-	let newPos = start + char.length;
-	clua.selectionStart = newPos;
-	clua.selectionEnd = newPos;
+		// Move cursor after inserted text
+		let newPos = start + char.length;
+		clua.selectionStart = newPos;
+		clua.selectionEnd = newPos;
+	});
 });
