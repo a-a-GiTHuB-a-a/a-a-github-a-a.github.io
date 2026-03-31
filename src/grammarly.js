9e63b93883a98942368c0343e489a0db0a0c47c2
a-a-GiTHuB-a-a <64599476+a-a-GiTHuB-a-a@users.noreply.github.com>
2026-03-31 09:22:41 -0400

    interlude

--- src/grammarly.js
+++ src/grammarly.js
@@ -4,6 +4,8 @@ $(function() {
 		"data-gramm_editor": false,
 		"data-enable-grammarly": false,
 		"spellcheck": false,
+		"autocapitalize": "none",
+		"autocorrect": "off",
 	};
 	$(".code,.no-grammar").attr(grammarly_disabler);
 });
